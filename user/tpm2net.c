5ce7afb5f99fcb2b9ce436ebbeaacb7cdfe793d0
Scott Franzyshen <sfranzyshen@gmail.com>
2015-02-17 08:29:07 -0800

    Just in case packetnumber = 0x00
    Just in case packetnumber = 0x00

--- user/tpm2net.c
+++ user/tpm2net.c
@@ -13,11 +13,11 @@ static void ICACHE_FLASH_ATTR tpm2net_recv(void *arg, char *pusrdata, unsigned s
     if (data && length >= 6 && data[0]==0x9C) { // header identifier (packet start)
         uint8_t blocktype = data[1]; // block type
         uint16_t framelength = ((uint16_t)data[2] << 8) | (uint16_t)data[3]; // frame length
-        uint8_t packagenum = data[4]; // packet number 1-255 
+        uint8_t packagenum = data[4]; // packet number 0-255 0x00 = no frame split
         uint8_t numpackages = data[5]; // total packets 1-255
         if (blocktype == 0xDA) { // data command ...
             if (length >= framelength + 7 && data[6+framelength]==0x36) { // header end (packet stop)
-                if (packagenum == 0x01 && numpackages == 0x01) { // no frame split found
+                if (numpackages == 0x01) { // no frame split found
                     unsigned char *frame = &data[6]; // pointer 'frame' to espconn's data (start of data)
                     ws2812_out(frame, framelength); // send data to strip
                 } else { //frame split is found
